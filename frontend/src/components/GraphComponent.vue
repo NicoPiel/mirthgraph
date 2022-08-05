aa5664ba1c286ea6fd3e12ef2d37c4ee19ec206a
Caigh <nico.piel@hotmail.de>
2022-08-05 16:17:24 +0200

    Changed directory structure and added server

---
+++ frontend/src/components/GraphComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import * as xml2js from 'xml2js';
+import {readFile} from 'fs';
+
+function readDataAndConvert() {
+  const parser = new xml2js.Parser();
+  readFile(__dirname + '/foo.xml', function (err, data) {
+    parser.parseString(data, function (err, result) {
+      console.dir(result);
+      console.log('Done');
+    });
+  });
+}
+
+export default defineComponent({
+  name: 'GraphComponent',
+  props: {},
+  setup() {
+    readDataAndConvert();
+    return null;
+  },
+});
+</script>
---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <graph-component
+      title="Example component"
+      active
+    ></graph-component>
+  </q-page>
+</template>
+
+<script lang="ts">
+import GraphComponent from 'components/GraphComponent.vue';
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {GraphComponent},
+  setup() {
+    return null;
+  }
+});
+</script>
