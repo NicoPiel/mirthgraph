e2706ef7a021668cd32d3ae4478e3d5b9f5368c3
Nico Piel <nico.piel@hotmail.de>
2024-09-03 18:26:45 +0200

    Fixed buttons

--- mirthgraph/Program.cs
+++ mirthgraph/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 using mirthgraph.Components;
 using Radzen;
@@ -6,18 +7,36 @@ using StackExchange.Redis;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorPages();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddServerSideBlazor();
 builder.Services.AddRadzenComponents();
-builder.Services.AddControllers();
+builder.Services.AddRadzenQueryStringThemeService();
+builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 builder.Services.AddDbContext<DbService>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAntiforgery(options => options.HeaderName = "X-CSRF-TOKEN");
 
+// Configure Data Protection to persist keys and use DPAPI for encryption
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(Path.Combine(builder.Environment.ContentRootPath, "keys")));
+
+// Configure Redis connection
 var redisConnection = builder.Configuration.GetConnectionString("RedisConnection");
-var redis = ConnectionMultiplexer.Connect(redisConnection);
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+{
+    var logger = sp.GetRequiredService<ILogger<Program>>();
+    try
+    {
+        return ConnectionMultiplexer.Connect(redisConnection);
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "Could not connect to Redis.");
+        throw;
+    }
+});
 
-builder.Services.AddSingleton<IConnectionMultiplexer>(redis);
 builder.Services.AddSingleton<CacheService>();
 builder.Services.AddSingleton<MirthConfigService>();
 builder.Services.AddScoped<GraphsService>();
@@ -27,25 +46,32 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbService = scope.ServiceProvider.GetRequiredService<DbService>();
-    dbService.Database.EnsureCreated();
+    if (app.Environment.IsDevelopment())
+    {
+        dbService.Database.EnsureCreated();
+    }
+    else
+    {
+        // For production, use migrations
+        dbService.Database.Migrate();
+    }
 }
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
-app.UseRouting();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseRouting();
 app.UseAntiforgery();
-
+app.MapRazorPages();
 app.MapControllers();
-
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.MapControllers();
 
 app.Run();
